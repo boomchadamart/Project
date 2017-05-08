3e171d5ad2aa75efd3e06e3227503269c2359be3
boomchadamart <gm5540117@gmail.com>
2017-05-09 01:01:40 +0700

    Revert "new"
    This reverts commit 7b0d005bc475722bed80d2e3dc1f8314f9d8c842.

--- login1/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset ( $_SESSION['session_id'] );
-unset ( $_SESSION['username'] );
-unset ( $_SESSION['status'] );
-session_destroy();
-echo "<meta http-equiv='refresh' content='1;URL=login.php'>";
-?>
